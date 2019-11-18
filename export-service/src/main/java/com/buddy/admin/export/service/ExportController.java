5b5f89d8b36fdb17a8d6927d248fc6c556c701d1
jchevs <cjayaraj2@gmail.com>
2019-11-18 19:01:16 +0530

    new controller

---
+++ export-service/src/main/java/com/buddy/admin/export/service/ExportController.java
@@ -0,0 +1,10 @@
+package com.buddy.admin.export.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/export")
+public class ExportController {
+
+}
